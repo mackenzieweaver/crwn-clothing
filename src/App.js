4c2262e88bde29685d3fb2403126609850276c72
Mack <macknz7@gmail.com>
2021-03-19 16:06:21 -0700

    shop page and preview collection components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-import { Switch, Route, Link } from 'react-router-dom';
+import ShopPage from "./pages/shop/shop.component";
+
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
   
         <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
 
     </div>
   );
