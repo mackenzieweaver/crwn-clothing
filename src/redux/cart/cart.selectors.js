21d9ee354722b58e3dec68cf61f28b25e3c81a18
Mack <macknz7@gmail.com>
2021-05-01 10:53:59 -0400

    checkout dropdown and checkout page add items, get total, display name

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -16,3 +16,9 @@ export const selectCartItemsCount = createSelector(
     [selectCartItems],
     (cartItems) => cartItems.reduce((accumulator, currentCartItem) => accumulator + currentCartItem.quantity, 0)
 );
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    (cartItems) => cartItems.reduce((accumulator, currentCartItem) => accumulator + currentCartItem.quantity * currentCartItem.price, 0)
+
+)
