ac3e79496e9da3b80cc6d861e7252ae2fb60e2c3
Mack <macknz7@gmail.com>
2021-09-25 18:29:08 -0400

    collection state into redux
    section 10 #145

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 import directoryReducer from './directory/directory.reducer';
-
+import shopReducer from './shop/shop.reducer';
 
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
@@ -16,7 +16,8 @@ const persistConfig = {
 const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
-    directory: directoryReducer
+    directory: directoryReducer,
+    shop: shopReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
